d98a488f8bb54ebde22af9bda05bc9369dffb3b2
Kean <Kean@DESKTOP-BNLIDJS>
2021-11-04 20:22:10 +0800

    Add project files.

---
+++ AppsDev-2/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace AppsDev_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.stdid = new System.Windows.Forms.TextBox();
+            this.firstname = new System.Windows.Forms.TextBox();
+            this.lastname = new System.Windows.Forms.TextBox();
+            this.quizscore = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.btnfind = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 315);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(548, 212);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Quiz Score:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Result:";
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(27, 266);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(148, 39);
+            this.btnsave.TabIndex = 6;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(234, 266);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(148, 39);
+            this.btnupdate.TabIndex = 7;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Visible = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(427, 266);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(148, 39);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Visible = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // stdid
+            // 
+            this.stdid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stdid.Location = new System.Drawing.Point(173, 32);
+            this.stdid.Name = "stdid";
+            this.stdid.Size = new System.Drawing.Size(242, 31);
+            this.stdid.TabIndex = 9;
+            this.stdid.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // firstname
+            // 
+            this.firstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstname.Location = new System.Drawing.Point(173, 74);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(242, 31);
+            this.firstname.TabIndex = 10;
+            // 
+            // lastname
+            // 
+            this.lastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastname.Location = new System.Drawing.Point(173, 115);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(242, 31);
+            this.lastname.TabIndex = 11;
+            // 
+            // quizscore
+            // 
+            this.quizscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quizscore.Location = new System.Drawing.Point(173, 157);
+            this.quizscore.Name = "quizscore";
+            this.quizscore.Size = new System.Drawing.Size(242, 31);
+            this.quizscore.TabIndex = 12;
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(173, 200);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(242, 31);
+            this.result.TabIndex = 13;
+            // 
+            // btnfind
+            // 
+            this.btnfind.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfind.Location = new System.Drawing.Point(421, 32);
+            this.btnfind.Name = "btnfind";
+            this.btnfind.Size = new System.Drawing.Size(77, 31);
+            this.btnfind.TabIndex = 14;
+            this.btnfind.Text = "Find";
+            this.btnfind.UseVisualStyleBackColor = true;
+            this.btnfind.Click += new System.EventHandler(this.btnfind_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(439, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 19);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Remarks";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(437, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 19);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "<75           POOR";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(439, 126);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 19);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "75 - 80     FAIR";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(438, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 19);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "81 - 90     GOOD";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(437, 172);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(163, 19);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "90>           EXCELLENT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 539);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnfind);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.quizscore);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.stdid);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.TextBox stdid;
+        private System.Windows.Forms.TextBox firstname;
+        private System.Windows.Forms.TextBox lastname;
+        private System.Windows.Forms.TextBox quizscore;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button btnfind;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ AppsDev-2/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppsDev_2
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Kean\source\repos\AppsDev-2\AppsDev-2\Database1.mdf;Integrated Security=True");
+            cn.Open();
+            GetAllStudentRecord();
+        }
+
+        private void GetAllStudentRecord()
+        {
+            cmd = new SqlCommand("Select * from tblStd", cn);
+            da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if(stdid.Text != "" || firstname.Text != "" || lastname.Text != "" || quizscore.Text != "" || result.Text != "")
+            {
+                saveInfo();
+
+            } 
+            else
+            {
+                MessageBox.Show("Please fill out all necesarry fields.");
+                stdid.Focus();
+            }
+            GetAllStudentRecord();
+        }
+
+        protected void saveInfo()
+        {
+            string QUERY = "INSERT INTO tblStd " +
+                "(stid, firstname, lastname, quizscore, result) " +
+                "VALUES (@stid, @firstname, @lastname, @quizscore, @result)";
+
+            SqlCommand CMD = new SqlCommand(QUERY, cn);
+            CMD.Parameters.AddWithValue("@stid", stdid.Text);
+            CMD.Parameters.AddWithValue("@firstname", firstname.Text);
+            CMD.Parameters.AddWithValue("@lastname", lastname.Text);
+            CMD.Parameters.AddWithValue("@quizscore", quizscore.Text);
+            CMD.Parameters.AddWithValue("@result", result.Text);
+            CMD.ExecuteNonQuery();
+        }
+
+        protected void updateInfo()
+        {
+            string QUERY = "Update tblStd " +
+                "Set firstname = @firstname, lastname = @lastname, quizscore = @quizscore, result = @result " +
+                "where stid = @stid";
+
+            SqlCommand CMD = new SqlCommand(QUERY, cn);
+            CMD.Parameters.AddWithValue("@stid", stdid.Text);
+            CMD.Parameters.AddWithValue("@firstname", firstname.Text);
+            CMD.Parameters.AddWithValue("@lastname", lastname.Text);
+            CMD.Parameters.AddWithValue("@quizscore", quizscore.Text);
+            CMD.Parameters.AddWithValue("@result", result.Text);
+            CMD.ExecuteNonQuery();
+        }
+
+        protected void deleteInfo()
+        {
+            string QUERY = "Delete from tblStd " +
+                "where stid = @stid";
+
+            SqlCommand CMD = new SqlCommand(QUERY, cn);
+            CMD.Parameters.AddWithValue("@stid", stdid.Text);
+            CMD.ExecuteNonQuery();
+        }
+
+        private void btnfind_Click(object sender, EventArgs e)
+        {
+
+            if (stdid.Text != "")
+            {
+                cmd = new SqlCommand("Select * from tblStd where stid =" + stdid.Text, cn);
+                da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                btnupdate.Visible = true;
+                btndelete.Visible = true;
+            } else
+            {
+                MessageBox.Show("Please enter valid ID");
+            }
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            updateInfo();
+            GetAllStudentRecord();
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            deleteInfo();
+            GetAllStudentRecord();
+        }
+    }
+}
